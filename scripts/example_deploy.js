9a330f17021ce9a37a091f97792ed9eaa9436148
Joey Hu <47383527+HjzQAQ@users.noreply.github.com>
2024-02-13 21:34:45 +0000

    Create example_deploy.js

---
+++ scripts/example_deploy.js
@@ -0,0 +1,19 @@
+// This is an example JavaScript file for deployment
+
+
+//async function main() {
+//    const [deployer] = await ethers.getSigners();
+  
+//    console.log("Deploying contracts with the account:", deployer.address);
+  
+//   const token = await ethers.deployContract("Token");
+  
+//    console.log("Token address:", await token.getAddress());
+// }
+  
+// main()
+//    .then(() => process.exit(0))
+//    .catch((error) => {
+//      console.error(error);
+//     process.exit(1);
+//});
