4a4bd91e5c3f66daf6c5e367d039f7cbdf30b48c
Joey Hu <47383527+HjzQAQ@users.noreply.github.com>
2024-02-13 21:30:30 +0000

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,23 @@
+require###;
+
+// Go to https://infura.io, sign up, create a new API key
+// in its dashboard, and replace "KEY" with it
+const INFURA_API_KEY = "KEY";
+
+// Replace this private key with your Sepolia account private key
+// To export your private key from Coinbase Wallet, go to
+// Settings > Developer Settings > Show private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Beware: NEVER put real Ether into testing accounts
+const SEPOLIA_PRIVATE_KEY = "KEY";
+
+module.exports = {
+  solidity: "0.8.23",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    }
+  }
+};
